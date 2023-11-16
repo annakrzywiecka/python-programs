0ce58daa80bbb4fe63de6c2a8555f53c28fcd857
annakrzywiecka <ania.krzywiecka@gmail.com>
2023-11-16 12:19:53 +0100

    zadania na 1 konwersatorium dodane

--- zadania_1_konwersatorium_python.py
+++ zadania_1_konwersatorium_python.py
@@ -1,5 +1,5 @@
 
-def statystyka_slowa(portion):
+def stats_of_abstract(portion):
     '''Program, który wyświetli liczbę zdań, liczbę słów, najdłuższe słowo, najczęściej występujące słowo o długości co najmniej 5 znaków.'''
     number_of_sentences = portion.count('.')    
     table_of_words = portion.split()
@@ -52,22 +52,17 @@ def reversing_words(abstract):
             text = text[:i] + " " + text[i] + " " + text[i+1:]
             i += 1
         i += 1
-
     output_text = " ".join([word[::-1] for word in text.split()])
-
     i = 0
     while i < len(output_text):                                    #usuwanie spacji
         if output_text[i] in [",",".",":",";","/","(",")"]:
             output_text = output_text[:i-1] + output_text[i:]
             i -= 1
         i += 1
-
-
     output_text = separate_text(output_text)
-
     return output_text
 
-def statistics_of_text(abstract):
+def lengths_of_text(abstract):
     """Program, który dla każdego zdania w abstract wygeneruje sekwencje długości kolejnych słów, wyświetli ją w jednym wierszu a na koniec policzy średnią długość słowa w analizowanym zdaniu."""
     text = abstract[:]
     i=0
@@ -88,18 +83,8 @@ def statistics_of_text(abstract):
 
 
 
-
-
-
-
-
-
-
-
-
-
 abstract = "Phospholipid membranes support essential biochemical processes, yet remain difficult to characterize due to their compositional and structural heterogeneity. The two most common phospholipid headgroup structures in biological membranes are phosphatidylcholine (PC) and phosphatidylethanolamine (PE), but interactions between PC and PE lipids remain underexplored. In this study, we apply ultrafast two-dimensional infrared (2D IR) spectroscopy to quantify the headgroup effects on interfacial dynamics in PC/PE lipid mixtures. Experiments are interpreted through molecular dynamics simulations using the molecular dynamics with alchemical step (MDAS) algorithm for enhanced sampling. Experimental results indicate that the PE content decreases H-bond formation at the ester carbonyl positions near the lipid membrane's hydrophobic core as a result of increased packing density. The observed dehydration is linked to faster molecular dynamics within the interfacial region."
-print(statystyka_slowa(abstract))
+print(stats_of_abstract(abstract))
 print(separate_text(abstract))
 print(reversing_words(abstract))
-print(statistics_of_text(abstract))
+print(lengths_of_text(abstract))
